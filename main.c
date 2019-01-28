50de0d035edddcccf238fa19becb2f038b1c2ee3
Kaio Rosa <kaiosrosa@gmail.com>
2019-01-28 15:54:04 +0000

    start reading files

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
-int main(void){
-    printf("This is gonna be a minesweeper in C!!!\n");
+int main(int argc, char* argv[]){
+    // Verificar Parementros
+    if(argc != 2){
+        printf("ERRO: O diretorio de arquivos de configuração nao foi informado!\n");
+        return 1;
+    }
+    // Inicializar o jogo
+    // ler arquivo tabuleiro.txt
+    FILE * filePointer = fopen(argv[1],"r");
+    
+    if(filePointer == NULL){
+        printf("Nao foi possivel ler o tabuleiro.txt em %s\n",argv[1]);
+        return 1;
+    }
+    // Pedir nome do usuario 
+    
+    //imprimir Estado inicial do tabuleiro 
+    
+    return 0;
 }
